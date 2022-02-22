557dd5465b336388b00193a2936f30f48a69ab06
Yureka <yuka@yuka.dev>
2022-02-22 19:27:08 +0100

    init password store

---
+++ pass.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if ! command -v pass; then
+	echo "install pass"
+	exit 1
+fi
+if ! command -v gpg; then
+	echo "install gpg"
+	exit 1
+fi
+
+cd $(dirname $0)
+
+for key in .gpg-public-keys/*
+do
+	gpg --import "$key" 2>/dev/null
+done
+
+export PASSWORD_STORE_GPG_OPTS="--trust-model always"
+export PASSWORD_STORE_DIR="$PWD/secrets"
+pass $@
