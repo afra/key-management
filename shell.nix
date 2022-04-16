2eb28c8a09ac4b6c732c705434ec604fda58c154
Yureka <yuka@yuka.dev>
2022-04-16 19:20:24 +0200

    shell.nix: nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> {};
+with import (fetchTarball https://github.com/nixos/nixpkgs/archive/b6966d911da89e5a7301aaef8b4f0a44c77e103c.tar.gz) {};
 
 mkShell {
   name = "key-management-shell";
