a109862f82a6eb7c13c258ab2beda0937acd9a72
Yureka <yuka@yuka.dev>
2022-04-16 19:12:16 +0200

    upload: door -> door.lan

--- scripts/upload.fish
+++ scripts/upload.fish
@@ -2,9 +2,9 @@
 
 cd (dirname (status filename))/..
 
-scp (./scripts/sshKeys.fish | psub) root@door:/home/open/.ssh/authorized_keys
+scp (./scripts/sshKeys.fish | psub) root@door.lan:/home/open/.ssh/authorized_keys
 
-scp (./scripts/sshKeys.fish | psub) root@door:/home/close/.ssh/authorized_keys
+scp (./scripts/sshKeys.fish | psub) root@door.lan:/home/close/.ssh/authorized_keys
 
-scp (./scripts/rfidTokens.fish | psub) root@door:/home/open/rfid_codes.txt
+scp (./scripts/rfidTokens.fish | psub) root@door.lan:/home/open/rfid_codes.txt
 
