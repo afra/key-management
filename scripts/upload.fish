4b4fea42250cd84081be520ff40bae4564aa93b8
Lilian <lilian@blahaj.space>
2024-01-27 16:55:37 +0100

    upload.fish: door.lan -> door.space.afra-berlin.de

--- scripts/upload.fish
+++ scripts/upload.fish
@@ -2,9 +2,9 @@
 
 cd (dirname (status filename))/..
 
-scp (./scripts/sshKeys.fish | psub) root@door.lan:/home/open/.ssh/authorized_keys
+scp (./scripts/sshKeys.fish | psub) root@door.space.afra-berlin.de:/home/open/.ssh/authorized_keys
 
-scp (./scripts/sshKeys.fish | psub) root@door.lan:/home/close/.ssh/authorized_keys
+scp (./scripts/sshKeys.fish | psub) root@door.space.afra-berlin.de:/home/close/.ssh/authorized_keys
 
-scp (./scripts/rfidTokens.fish | psub) root@door.lan:/home/open/rfid_codes.txt
+scp (./scripts/rfidTokens.fish | psub) root@door.space.afra-berlin.de:/home/open/rfid_codes.txt
 
